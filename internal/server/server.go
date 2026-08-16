e58497cf6fbe27e48851b5153158042d939814e6
Dzedz Timofei <dzedz@internet.ru>
2026-08-16 18:43:00 +0300

    Server download file function

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,10 +3,22 @@ package server
 import (
 	"godrop/internal/qrgen"
 	"net/http"
+	"path"
 )
 
-func StartServer() {
+func StartServerDownload(filepath string) {
 	ip := GetOutboundIP()
+	filename := path.Base(filepath)
+
 	qrgen.QrGen("http://" + ip.String() + ":8080")
-	http.ListenAndServe(ip.String()+":8080", http.FileServer(http.Dir("static")))
+
+	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Header().Set("Content-Disposition", "attachment; filename="+filename)
+		http.ServeFile(w, r, filepath)
+	})
+
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	http.ListenAndServe(ip.String()+":8080", nil)
 }
