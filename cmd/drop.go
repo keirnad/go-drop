8510c233de4d42cb1ac15cf29e628dab0ec9b7c2
Dzedz Timofei <dzedz@internet.ru>
2026-08-16 18:42:50 +0300

    Fucntion drop

--- cmd/drop.go
+++ cmd/drop.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"godrop/internal/server"
+	"log"
 
 	"github.com/spf13/cobra"
 )
@@ -20,10 +21,15 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		server.StartServer()
+		path, err := cmd.Flags().GetString("path")
+		if err != nil {
+			log.Println(err)
+		}
+		server.StartServerDownload(path)
 	},
 }
 
 func init() {
+	dropCmd.Flags().String("path", "", "Set path of file")
 	rootCmd.AddCommand(dropCmd)
 }
